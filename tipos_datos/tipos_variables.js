1613ea8c8bdf9e4f54a8886b51c37ab45ba24f05
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-11-08 22:19:20 -0500

    Update tipos_variables.js

--- tipos_datos/tipos_variables.js
+++ tipos_datos/tipos_variables.js
@@ -4,3 +4,10 @@ console.log(nombre);
 //dato numerico
 var numero = 1000;
 console.log(numero);
+
+//dato objeto
+var objeto = {
+    nombre: "Jhon",
+    apellido: "Gomez",
+    telefono: "1234567890"
+}
