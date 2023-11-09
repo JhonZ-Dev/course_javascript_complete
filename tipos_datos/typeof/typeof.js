22aab21ce3250ea5ad63a4eb1f168f6384004fe3
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-11-08 22:36:32 -0500

    Updates

--- tipos_datos/typeof/typeof.js
+++ tipos_datos/typeof/typeof.js
@@ -2,3 +2,6 @@
 var nombre = "Jhon";
 console.log(nombre);
 console.log(typeof nombre); //indica que tipo de dato se esta utilizando
+
+nombre = 10.5;
+console.log(typeof nombre); // cambio de tipo de dato
